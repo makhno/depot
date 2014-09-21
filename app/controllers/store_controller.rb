b0f7a50babdc7421da587f2c0d74ae5a4e53ec9e
Vladimir Makhnovskiy <makhnovskiy1@yahoo.com>
2014-09-20 23:23:42 -0400

    page 120

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+      session[:counter] += 1
+    end
+    
+    @page_accessed_counter = session[:counter]
   end
 end
